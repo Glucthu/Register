d4854218e464d6c3022786fb0b99f5f2267f7c18
Glucthu <glucthu@protonmail.com>
2021-08-31 15:25:05 -0300

    added style

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 function verifyForm()
 {
-    let password = document.getElementById("password").value;
-    let retypePassword = document.getElementById("retype password").value;
-    if (password != retypePassword)
+    if (verifyEmail() && verifyPassword()){
+        window.location = "Registered.html";
+    }
+}
+
+function verifyEmail(){
+    let email = document.getElementById("email").value;
+    let emailRegex = /^\w+@\w+\.[a-z]+/;
+
+    if (!emailRegex.test(email))
     {
-        document.getElementById("password").style.border = "rgb(170, 20, 20) 1px solid";
-        document.getElementById("retype password").style.border = "rgb(170, 20, 20) 1px solid";
+        document.getElementById("email").style.borderColor = "rgb(226, 33, 33)";
+        document.getElementById("error").innerHTML = "Email format: abc@def.xyz";
+        return false;
+    } else {
+        document.getElementById("email").style.borderColor = "rgb(129, 129, 129)";
+        return true;
+    }
+}
+
+function verifyPassword(){
+    let password = document.getElementById("password").value;
+    let retypePassword = document.getElementById("retype-password").value;
+
+    if(password.length < 4){
+        document.getElementById("password").style.borderColor = "rgb(226, 33, 33)";
+        document.getElementById("error").innerHTML = "Minimum password length: 4";
+        return false;
+    } else if (password != retypePassword) {
+        document.getElementById("password").style.borderColor = "rgb(226, 33, 33)";
+        document.getElementById("retype-password").style.borderColor = "rgb(226, 33, 33)";
         document.getElementById("error").innerHTML = "Passwords didn't match";
+        return false;
+    } else {
+        document.getElementById("password").style.borderColor = "rgb(129, 129, 129)";
+        document.getElementById("retype-password").style.borderColor = "rgb(129, 129, 129)";
+        return true
     }
 }
